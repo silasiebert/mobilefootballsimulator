8cc4ce5ee352b35f0e3d0069a3e5c153d8bd8e6c
Sila Georges Agiru Judick Siebert <sila.siebert@gmail.com>
2016-11-10 02:02:40 -0200

    Classe estadio desenvolvida por Wagner

--- BrasFootMobile/app/src/main/java/com/example/sila/brasfootmobile/Estadio.java
+++ BrasFootMobile/app/src/main/java/com/example/sila/brasfootmobile/Estadio.java
@@ -1,6 +1,10 @@
 package com.example.sila.brasfootmobile;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,10 +13,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import Model.Clube;
+import Model.Jogador;
 
 public class Estadio extends AppCompatActivity {
     private TextView tvValorMostrado;
     private EditText valorRecebido;
+    private Editable texto;
+    private int ss;
+    private int finalValor;
+    private SQLiteDatabase db;
+    private static final String ARQUIVO_PREFERENCIAS = "arquivo_preferencias";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +35,65 @@ public class Estadio extends AppCompatActivity {
         setContentView(R.layout.activity_estadio);
         tvValorMostrado= (TextView) findViewById(R.id.tvValorMostrado);
         valorRecebido = (EditText) findViewById(R.id.etValorDigitado);
+        db = openOrCreateDatabase("foot", MODE_PRIVATE, null);
 
     }
 
-    public void test(View v){
-        Editable texto = this.valorRecebido.getText();
-        String ss = texto.toString();
+
+    public void calcular(View v){
+        texto = this.valorRecebido.getText();
+        ss = Integer.parseInt(texto.toString());
+        finalValor = ss*500;
 
 
-        tvValorMostrado.setText(ss);
+
+        tvValorMostrado.setText("R$: "+ finalValor);
     }
 
-    public void calcular(){
+    public void confirmar(View v) {
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Deseja Confirmar sua Compra de " + ss + " lugares por " + finalValor + "?");
+        alertDialogBuilder.setPositiveButton("Sim",
+                new DialogInterface.OnClickListener() {
 
 
+
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Cursor c = db.rawQuery("SELECT clubeId, caixa from clube WHERE" +
+                                " nome='" + getSharedPreferences(ARQUIVO_PREFERENCIAS, 0).getString("clube", "") + "'", null);
+                        c.moveToFirst();
+                        int meuClubeId = c.getInt(c.getColumnIndex("clubeId"));
+                        double caixa = c.getInt(c.getColumnIndex("caixa"));
+                        c.close();
+                        double novoCaixa=0;
+                        if(caixa>=finalValor) {
+                            novoCaixa=caixa-finalValor;
+                            db.execSQL("UPDATE clube SET caixa = '"+novoCaixa+"' WHERE clubeId = '" + meuClubeId + "';");
+                            Toast.makeText(getApplicationContext(), ss  +" lugares construído", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), finalValor + " não disponível. Valor em Caixa: " + caixa, Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("Cancelar",
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Toast.makeText(getApplicationContext(), "Ação NÃO confirmada!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -65,8 +123,20 @@ public class Estadio extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Estadio.class));
                 finish();
                 break;
+            case R.id.menuJogos:
+                startActivity(new Intent(getApplicationContext(), JogosActivity.class));
+                finish();
+                break;
+            case R.id.menuResultados:
+                startActivity(new Intent(getApplicationContext(), ResultadosActivity.class));
+                finish();
+                break;
+
+            case R.id.menuJogador:
+                startActivity(new Intent(getApplicationContext(), JogadorActivity.class));
+                finish();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 }
-
